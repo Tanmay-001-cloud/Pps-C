c2616833e077eadc27282823b9a5fca36b299253
Tanmay-001-cloud <bedsetanmay06@gmail.com>
2025-06-03 15:50:39 +0530

    Add files via upload

---
+++ 2D array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int mat[3][4], i, j;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("enter_number_of[%d][%d]:", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
